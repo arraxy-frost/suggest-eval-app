2a8fd1cd5797369d71d15048a66873fd382a4045
arraxy-frost <artyom.prilip@gmail.com>
2025-10-08 02:50:55 +0600

    feat(client): update vite config, enable static serving from public dir, update gitignore

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express from "express";
+import { Request, Response } from "express";
 import cors from "cors";
 import votingRouter from "./routes/voting.router";
 import suggestionsRouter from "./routes/suggestions.router";
+import path from "path";
 
 const app = express();
 
@@ -9,8 +11,12 @@ app.set('trust proxy', true);
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/api/votes', votingRouter);
 app.use('/api/suggestions', suggestionsRouter);
+app.get('/', (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 export default app;
